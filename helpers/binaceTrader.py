7c5285bb0d3d198c906007cebde9c6b882585dc9
Animus <lovro@x-network.io>
2022-01-01 20:25:53 +0200

    expanded binance trader class

--- helpers/binaceTrader.py
+++ helpers/binaceTrader.py
@@ -1,5 +1,7 @@
 from binance import Client
 from binance.exceptions import BinanceAPIException
+from colorama import Fore, init
+import math
 
 
 class BinanceTrader:
@@ -8,7 +10,68 @@ class BinanceTrader:
         self.private_key = private_key
         self.client = Client(api_key=self.public_key, api_secret=self.private_key)
 
-    def get_my_trades(self,symbol, order_id):
+    @staticmethod
+    def float_precision(f, n):
+        """
+        Calculate float precisions based on exchange data
+        """
+        n = int(math.log10(1 / float(n)))
+        f = math.floor(float(f) * 10 ** n) / 10 ** n
+        f = "{:0.0{}f}".format(float(f), n)
+        print(Fore.LIGHTCYAN_EX + f"{f}")
+        return str(int(f)) if int(n) == 0 else f
+
+    def get_buy_grid(self, grid_count, asset_price, step, step_size, value_per_mesh):
+        """
+        Calculates the grid
+        :param grid_count:
+        :param asset_price:
+        :param step:
+        :return:
+        """
+        levels = []
+        lvl = 1
+        step_value = 0
+        print(Fore.LIGHTGREEN_EX + f"GRID data...")
+        for x in range(grid_count):
+            step_value += asset_price * step
+            grid_level = asset_price - step_value
+            coin_currency_quantity = float(self.float_precision(value_per_mesh / grid_level, step_size))
+            lot_size = str(step_size).index('1') - 1
+            if lot_size < 0:
+                coin_currency_quantity = int(coin_currency_quantity)
+
+            print(f"L{lvl} @ {grid_level:.8f} USDT SUM: {coin_currency_quantity}")
+            lvl += 1
+            grid_value = {"qty": coin_currency_quantity,
+                          "levelPrice": grid_level}
+            levels.append(grid_value)
+        return levels
+
+    def get_limit_sell_info(self, price, gain_perc, coin_quantity, tick_size, step_size):
+        """
+        Get the data on limit sell
+        :return:
+        """
+        target = price * (1.00 + gain_perc)
+        price = float(self.float_precision(target, tick_size))
+        coin_currency_quantity = float(self.float_precision(coin_quantity, step_size))
+        return price, coin_currency_quantity
+
+    def get_tick_and_step_size(self, symbol_data):
+        """
+        Process symbol data
+        """
+        tick_size = None
+        step_size = None
+        for filt in symbol_data['filters']:
+            if filt['filterType'] == 'PRICE_FILTER':
+                tick_size = float(filt['tickSize'])
+            elif filt['filterType'] == 'LOT_SIZE':
+                step_size = float(filt['stepSize'])
+        return tick_size, step_size
+
+    def get_my_trades(self, symbol, order_id):
         return self.client.get_my_trades(symbol=symbol, orderId=order_id)[0]
 
     def get_orderbook_data(self, symbol):
